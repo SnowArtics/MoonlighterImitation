5b6a7462b71e92678309311bf23c0eb6f8ecf050
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2023-08-01 17:01:19 +0900

    feat : create sword Collider
    create sword Collider

--- SnowEngine/snIdleState.cpp
+++ SnowEngine/snIdleState.cpp
@@ -1,46 +1,52 @@
 #include "snIdleState.h"
-#include "snPlayerFSM.h"
-#include "snTime.h"
-#include "snInput.h"
+#include "..\Engine_SOURCE\snTime.h"
+#include "..\Engine_SOURCE\snInput.h"
 
-	IdleState::IdleState()
-		: PlayerState(PLAYER_STATE::IDLE)
-	{
-	}
-	IdleState::~IdleState()
-	{
-	}
-	void IdleState::Update()
-	{
-		if (Input::GetKey(eKeyCode::A))
-		{
-			GetPlayerFSM()->ChangeState(PLAYER_STATE::MOVE);
-			GetPlayerFSM()->SetDirection(PLAYER_DIR::LEFT);
-		}
-		if (Input::GetKey(eKeyCode::D))
-		{
-			GetPlayerFSM()->ChangeState(PLAYER_STATE::MOVE);
-			GetPlayerFSM()->SetDirection(PLAYER_DIR::RIGHT);
-		}
-		if (Input::GetKey(eKeyCode::S))
-		{
-			GetPlayerFSM()->ChangeState(PLAYER_STATE::MOVE);
-			GetPlayerFSM()->SetDirection(PLAYER_DIR::DOWN);
-		}
-		if (Input::GetKey(eKeyCode::W))
-		{
-			GetPlayerFSM()->ChangeState(PLAYER_STATE::MOVE);
-			GetPlayerFSM()->SetDirection(PLAYER_DIR::UP);
-		}
+IdleState::IdleState()
+	: PlayerState(PLAYER_STATE::IDLE)
+{
+}
 
-		if (Input::GetKey(eKeyCode::SPACE))
-			GetPlayerFSM()->ChangeState(PLAYER_STATE::ROLL);		
-	}
-	void IdleState::Enter()
+IdleState::~IdleState()
+{
+}
+
+void IdleState::Update()
+{
+	if (Input::GetKey(eKeyCode::A))
 	{
+		GetPlayerFSM()->ChangeState(PLAYER_STATE::MOVE);
+		GetPlayerFSM()->SetDirection(PLAYER_DIR::LEFT);
 	}
-	void IdleState::Exit()
+	if (Input::GetKey(eKeyCode::D))
 	{
+		GetPlayerFSM()->ChangeState(PLAYER_STATE::MOVE);
+		GetPlayerFSM()->SetDirection(PLAYER_DIR::RIGHT);
+	}
+	if (Input::GetKey(eKeyCode::S))
+	{
+		GetPlayerFSM()->ChangeState(PLAYER_STATE::MOVE);
+		GetPlayerFSM()->SetDirection(PLAYER_DIR::DOWN);
+	}
+	if (Input::GetKey(eKeyCode::W))
+	{
+		GetPlayerFSM()->ChangeState(PLAYER_STATE::MOVE);
+		GetPlayerFSM()->SetDirection(PLAYER_DIR::UP);
 	}
 
+	if (Input::GetKey(eKeyCode::SPACE))
+		GetPlayerFSM()->ChangeState(PLAYER_STATE::ROLL);
 
+	if (Input::GetKeyDown(eKeyCode::J) && GetPlayerFSM()->GetWeaponType() == WEAPON_TYPE::SHORT_SWORD)
+		GetPlayerFSM()->ChangeState(PLAYER_STATE::ATTSWORD1);
+	if (Input::GetKeyDown(eKeyCode::J) && GetPlayerFSM()->GetWeaponType() == WEAPON_TYPE::BOW)
+		GetPlayerFSM()->ChangeState(PLAYER_STATE::ATTBOW);
+}
+
+void IdleState::Enter()
+{
+}
+
+void IdleState::Exit()
+{
+}
